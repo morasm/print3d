3e965e3698a2a61f238125cf008f5ef88d20a4ca
Michał Morawski <moras.technika@gmail.com>
2020-10-18 01:56:05 +0200

    Added properties and application file

---
+++ src/main/java/com/moras/print3d/Print3dApplication.java
@@ -0,0 +1,13 @@
+package com.moras.print3d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Print3dApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Print3dApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moras/print3d/Print3dApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moras.print3d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Print3dApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
