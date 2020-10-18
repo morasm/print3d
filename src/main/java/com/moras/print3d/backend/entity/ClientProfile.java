092fb500d9e6bd2b2a8f27f6c771d154b4986ecb
Michał Morawski <moras.technika@gmail.com>
2020-10-18 12:52:57 +0200

    Added Entity ClientProfile

---
+++ src/main/java/com/moras/print3d/backend/entity/ClientProfile.java
@@ -0,0 +1,63 @@
+package com.moras.print3d.backend.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ClientProfile")
+public class ClientProfile {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "clientId")
+	private int clientId;
+	
+	@Column(name = "clientName")
+	private String clientName;
+	
+	@Column(name = "emailAddress")
+	private String emailAddress;
+	
+	public ClientProfile() {}
+
+	public ClientProfile(int clientId, String clientName, String emailAddress) {
+		this.clientId = clientId;
+		this.clientName = clientName;
+		this.emailAddress = emailAddress;
+	}
+
+	public int getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(int clientId) {
+		this.clientId = clientId;
+	}
+
+	public String getClientName() {
+		return clientName;
+	}
+
+	public void setClientName(String clientName) {
+		this.clientName = clientName;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	@Override
+	public String toString() {
+		return "ClientProfile [clientId=" + clientId + ", clientName=" + clientName + ", emailAddress=" + emailAddress
+				+ "]";
+	}
+	
+}
