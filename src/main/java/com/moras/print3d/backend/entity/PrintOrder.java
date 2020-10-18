414d6a4570ec305707d55b4ab4383268cbe4221f
Michał Morawski <moras.technika@gmail.com>
2020-10-18 12:54:10 +0200

    Added Entity PrintOrder

---
+++ src/main/java/com/moras/print3d/backend/entity/PrintOrder.java
@@ -0,0 +1,75 @@
+package com.moras.print3d.backend.entity;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PrintOrder")
+public class PrintOrder {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "orderId")
+	private int orderId;
+	
+	private ClientProfile clientProfile;
+	
+	private List<FileData> files;
+	
+	@Column(name= "orderStatus")
+	private OrderStatus orderStatus;
+
+	public PrintOrder() {};
+	
+	public PrintOrder(int orderId, ClientProfile clientProfile, List<FileData> files, OrderStatus orderStatus) {
+		this.orderId = orderId;
+		this.clientProfile = clientProfile;
+		this.files = files;
+		this.orderStatus = orderStatus;
+	}
+
+	public int getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+
+	public ClientProfile getClientProfile() {
+		return clientProfile;
+	}
+
+	public void setClientProfile(ClientProfile clientProfile) {
+		this.clientProfile = clientProfile;
+	}
+
+	public List<FileData> getFiles() {
+		return files;
+	}
+
+	public void setFiles(List<FileData> files) {
+		this.files = files;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return orderStatus;
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+
+	@Override
+	public String toString() {
+		return "PrintOrder [orderId=" + orderId + ", clientProfile=" + clientProfile + ", files=" + files
+				+ ", orderStatus=" + orderStatus + "]";
+	}
+	
+}
